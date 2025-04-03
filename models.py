0fcc0128c6787e9119a9f1100aaebe7c4f2715a8
Oluwapelumi Kehinde <63884321+SirPelzy@users.noreply.github.com>
2025-04-03 18:50:06 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,9 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime # We'll likely need this
 
+# Initialize SQLAlchemy instance
+# We associate it with the app in main.py
+db = SQLAlchemy()
+
+# --- Model definitions will go here later ---
