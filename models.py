453a975061642f3133131cc39c85643497727e88
Oluwapelumi Kehinde <63884321+SirPelzy@users.noreply.github.com>
2025-04-04 07:09:39 +0100

    Update models.py

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False, index=True)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     first_name = db.Column(db.String(50), nullable=True) # Allow null initially
     last_name = db.Column(db.String(50), nullable=True) # Allow null initially
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
